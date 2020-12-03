a10eb071bc4768c2e3b1048eacd880cbd61191dd
hsorby <h.sorby@auckland.ac.nz>
2020-12-03 23:39:51 +1300

    Change Git url to be a string.

--- tutorial1.cmake
+++ tutorial1.cmake
@@ -1,6 +1,6 @@
-set(TEST_GIT_REPO https://github.com/kerimoyle/libcellml-tutorials)
-set(TEST_COMMIT_HASH 6bb31e41230855af1150ec1f7cba0b02b08fd37e)
+set(TEST_GIT_REPO "https://github.com/kerimoyle/libcellml-tutorials")
+set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
 set(TEST_TARGETS tutorial1)
-set(TEST_CMAKELISTS_TXT_DIR tutorials/tutorial1)
+set(TEST_CMAKELISTS_DIR tutorials/tutorial1)
 #set(TEST_EXPECTED_RESULTS src/expected_results/)
 
