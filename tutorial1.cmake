8f45202e19279bfc8f3fe701f0d8f9e1e2c6a49a
hsorby <h.sorby@auckland.ac.nz>
2020-12-04 10:44:56 +1300

    Add command line argument to tutorial1.

--- tutorial1.cmake
+++ tutorial1.cmake
@@ -3,6 +3,7 @@ set(TEST_GIT_REPO "https://github.com/hsorby/libcellml-tutorials")
 #set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
 set(TEST_COMMIT_HASH "89e58349041df88cb4ef3db3ecf5e1a1f91b0a7e")
 set(TEST_TARGETS tutorial1_complete)
+set(TEST_TARGETS_ARGS tutorials/tutorial1/tutorial1.cellml)
 set(TEST_CMAKELISTS_DIR tutorials/tutorial1)
 #set(TEST_EXPECTED_RESULTS src/expected_results/)
 
