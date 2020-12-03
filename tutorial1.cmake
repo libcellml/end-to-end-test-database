963f2ff86384cce8e717211ff1b7632ac83bc408
hsorby <h.sorby@auckland.ac.nz>
2020-12-04 09:47:13 +1300

    Change to using a complete target.

--- tutorial1.cmake
+++ tutorial1.cmake
@@ -1,8 +1,8 @@
 #set(TEST_GIT_REPO "https://github.com/kerimoyle/libcellml-tutorials")
 set(TEST_GIT_REPO "https://github.com/hsorby/libcellml-tutorials")
 #set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
-set(TEST_COMMIT_HASH "2baae64dd98c2e541a881d2b2e09243275d4b239")
-set(TEST_TARGETS tutorial1)
+set(TEST_COMMIT_HASH "1017d9271c6ea26e53bdef2d2a63e102ee5c1f6e")
+set(TEST_TARGETS tutorial1_complete)
 set(TEST_CMAKELISTS_DIR tutorials/tutorial1)
 #set(TEST_EXPECTED_RESULTS src/expected_results/)
 
