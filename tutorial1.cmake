628cc94d38e7221f7373b260e0a0f7dcf453f4cf
hsorby <h.sorby@auckland.ac.nz>
2020-12-04 09:58:33 +1300

    Update commit hash for tutorial1.

--- tutorial1.cmake
+++ tutorial1.cmake
@@ -1,7 +1,7 @@
 #set(TEST_GIT_REPO "https://github.com/kerimoyle/libcellml-tutorials")
 set(TEST_GIT_REPO "https://github.com/hsorby/libcellml-tutorials")
 #set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
-set(TEST_COMMIT_HASH "1017d9271c6ea26e53bdef2d2a63e102ee5c1f6e")
+set(TEST_COMMIT_HASH "89e58349041df88cb4ef3db3ecf5e1a1f91b0a7e")
 set(TEST_TARGETS tutorial1_complete)
 set(TEST_CMAKELISTS_DIR tutorials/tutorial1)
 #set(TEST_EXPECTED_RESULTS src/expected_results/)
