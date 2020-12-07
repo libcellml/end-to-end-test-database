0bfe2b5cdd37ac9d503c00c23426ef0c561c852b
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-12-08 10:44:35 +1300

    Update tutorial1.cmake
    Set expected results and update commit hash.

--- tutorial1.cmake
+++ tutorial1.cmake
@@ -1,9 +1,8 @@
 #set(TEST_GIT_REPO "https://github.com/kerimoyle/libcellml-tutorials")
 set(TEST_GIT_REPO "https://github.com/hsorby/libcellml-tutorials")
 #set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
-set(TEST_COMMIT_HASH "89e58349041df88cb4ef3db3ecf5e1a1f91b0a7e")
+set(TEST_COMMIT_HASH "37cd7e415117f965f799bf94346eefdb6affd560")
 set(TEST_TARGETS tutorial1_complete)
 set(TEST_TARGETS_ARGS tutorials/tutorial1/tutorial1.cellml)
 set(TEST_CMAKELISTS_DIR tutorials/tutorial1)
-#set(TEST_EXPECTED_RESULTS src/expected_results/)
-
+set(TEST_EXPECTED_RESULTS tutorials/tutorial1/expected_results/)
