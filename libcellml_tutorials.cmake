5a35503f46cf3b36609bcb854350eb7c97096692
hsorby <h.sorby@auckland.ac.nz>
2020-12-08 13:30:11 +1300

    Fixes for running tutorials locally.

---
+++ libcellml_tutorials.cmake
@@ -0,0 +1,9 @@
+#set(TEST_GIT_REPO "https://github.com/kerimoyle/libcellml-tutorials")
+set(TEST_GIT_REPO "/Users/hsor001/Projects/cellml/examples/libcellml-tutorials")
+#set(TEST_COMMIT_HASH "6bb31e41230855af1150ec1f7cba0b02b08fd37e")
+set(TEST_COMMIT_HASH "")
+set(TEST_TARGETS tutorial1_complete tutorial2_complete)
+set(TEST_TARGETS_ARGS tutorials/tutorial1/tutorial1.cellml tutorials/tutorial2/tutorial2.cellml)
+set(TEST_CMAKELISTS_DIR tutorials/tutorial1 tutorials/tutorial2)
+set(TEST_EXPECTED_RESULTS tutorials/tutorial1/expected_results/ "NOTFOUND")
+
